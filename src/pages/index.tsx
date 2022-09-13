7fc023b4d8c24c707d03f0b5ddd6602f1ece0713
Duncan <43674322+DeVlaminckDuncan@users.noreply.github.com>
2022-09-13 16:29:29 +0200

    Replace hard coded title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
+import { graphql, useStaticQuery } from 'gatsby';
 import * as React from 'react';
 import Container from '../common/container';
 
 const IndexPage = () => {
+  const {
+    site: {
+      siteMetadata: { title },
+    },
+  } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `,
+  );
+
   return (
     <Container>
-      <title>Duncan De Vlaminck</title>
-      <h1>Duncan De Vlaminck</h1>
+      <title>{title}</title>
+      <h1>{title}</h1>
     </Container>
   );
 };
